03167b8c860958c5ac82502c6260777262d40fb6
GustavM01 <gustav.milihagulu@gmail.com>
2026-06-01 00:19:30 +1000

    HeroGraphic mouse tracking

--- src/components/Hero/HeroGraphic.jsx
+++ src/components/Hero/HeroGraphic.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./HeroGraphic.css";
 import {
   CodeXml,
@@ -8,44 +8,78 @@ import {
 } from "lucide-react";
 
 function HeroGraphic() {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [position, setPosition] = useState({ x: -15, y: 10 });
+  const [isReturning, setIsReturning] = useState(false);
+  const cardRef = useRef(null);
+  const wasInside = useRef(false);
 
-  //   useEffect(() => {
-  //     const handleMouseMovement = (event) => {
-  //       setPosition({
-  //         x: event.clientX,
-  //         y: event.clientY,
-  //       });
-  //       //   console.log(e);
-  //     };
-  //     console.log(position);
-  //     window.addEventListener("mousemove", handleMouseMovement);
+  useEffect(() => {
+    const handleMouseMovement = (e) => {
+      const bounds = cardRef.current.getBoundingClientRect();
 
-  //     return () => window.removeEventListener("mousemove", handleMouseMovement);
-  //   }, []);
-  //   console.log(position);
+      const xCenter = bounds.width / 2;
+      const yCenter = bounds.height / 2;
 
-  const handleMouse = (e) => {
-    const bounds = e.currentTarget.getBoundingClientRect();
+      const padding = 200;
+      const maxRotation = 25;
 
-    const xCenter = bounds.width / 2;
-    const yCenter = bounds.height / 2;
+      const left = bounds.left - padding;
+      const right = bounds.right + padding;
+      const top = bounds.top - padding;
+      const bottom = bounds.bottom + padding;
 
-    const x = (e.clientX - bounds.left - xCenter) / 6;
-    const y = ((e.clientY - bounds.top - yCenter) / 6) * -1;
+      const insideZone =
+        e.clientX >= left &&
+        e.clientX <= right &&
+        e.clientY >= top &&
+        e.clientY <= bottom;
 
-    setPosition({ x, y });
-  };
+      const calculatedX = (e.clientX - bounds.left - xCenter) / 6;
+      const calculatedY = ((e.clientY - bounds.top - yCenter) / 6) * -1;
+
+      const x = Math.max(-maxRotation, Math.min(maxRotation, calculatedX));
+
+      const y = Math.max(-maxRotation, Math.min(maxRotation, calculatedY));
+
+      if (insideZone) {
+        setIsReturning(false);
+        setPosition({ x, y });
+        wasInside.current = true;
+      } else {
+        setIsReturning(true);
+        setPosition({ x: -15, y: 10 });
+        wasInside.current = false;
+      }
+    };
+
+    window.addEventListener("mousemove", handleMouseMovement);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMovement);
+    };
+  }, []);
+
+  //   const handleMouse = (e) => {
+  //     const bounds = e.currentTarget.getBoundingClientRect();
+
+  //     const xCenter = bounds.width / 2;
+  //     const yCenter = bounds.height / 2;
+
+  //     const x = (e.clientX - bounds.left - xCenter) / 6;
+  //     const y = ((e.clientY - bounds.top - yCenter) / 6) * -1;
+
+  //     setPosition({ x, y });
+  //   };
 
   return (
     <>
-      <div
-        onMouseMove={handleMouse}
-        onMouseLeave={() => setPosition({ x: 0, y: 0 })}
-        className="scene"
-      >
+      {/* <div style={{ position: "absolute", left: 500, top: 100 }}>
+        <p>x: {position.x}</p>
+        <p>y: {position.y}</p>
+      </div> */}
+      <div ref={cardRef} className="scene">
         <div
-          className="graphic"
+          className={isReturning ? "graphic returning" : "graphic"}
           style={{
             "--rotate-x": `${position.y}deg`,
             "--rotate-y": `${position.x}deg`,
